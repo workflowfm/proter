ee303314db1a9422b9c40bcd017a564ea709136a
zedrox <zedrox0@gmail.com>
2020-07-25 12:16:16 +0100

    fixing using same task multiple times

--- flowsExample/src/main/scala/com/workflowfm/simulator/flowsExample/FlowsMain.scala
+++ flowsExample/src/main/scala/com/workflowfm/simulator/flowsExample/FlowsMain.scala
@@ -14,7 +14,7 @@ import java.io.File
 
 object FlowsMain {
     //toggle for debug
-    val DEBUG = true
+    val DEBUG = false
 
     def main(args: Array[String]): Unit = {
 
@@ -68,13 +68,15 @@ object FlowsMain {
         // val flow2 = And(task2,And(task3,And(task4,And(task5,And(task6,NoTask)))))
         // val flow3 = All(task1,task2,task3,task4,task5,task6,task7)
         // val flow4 = And(Then(task5,task1),Then(task2,task3))
+        
         val flow = Just(task1)
-        val flow2 = task1 > task2
+        val flow2 = task3 > task2 > task3
+
 
         val generator = new ConstantGenerator[Long](3L)
-        //coordinator ! Coordinator.AddSim(0L,system.actorOf(FlowSimulationActor.props("sim1",coordinator,flow5),"sim1"))
-        coordinator ! Coordinator.AddSim(15L,system.actorOf(FlowSimulationActor.props("sim2",coordinator,flow2),"sim2"))
-        coordinator ! Coordinator.AddSim(20L,system.actorOf(FlowSimulationActor.props("sim1",coordinator,flow),"sim1"))
+        //coordinator ! Coordinator.AddSim(0L,system.actorOf(FlowSimulationActor.props("sim1",coordinator,flow4),"sim1"))
+        coordinator ! Coordinator.AddSim(0L,system.actorOf(FlowSimulationActor.props("sim2",coordinator,flow2),"sim2"))
+        //coordinator ! Coordinator.AddSim(0L,system.actorOf(FlowSimulationActor.props("sim1",coordinator,flow),"sim1"))
         coordinator ! Coordinator.Start
     }
 
