3623f2b05df8349152043ff4d064ed028e082071
MBaczun <s1743497@ed.ac.uk>
2020-08-13 13:03:23 +0100

    scalafmt formating

--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
-ThisBuild / version          := "0.3-SNAPSHOT"
-ThisBuild / autoAPIMappings  := true
+ThisBuild / version := "0.3-SNAPSHOT"
+ThisBuild / autoAPIMappings := true
 
 // Fixes some sbt import problems.
 // https://github.com/sbt/sbt-native-packager/issues/1063
@@ -10,39 +10,37 @@ scalacOptions in (Compile, doc) += "-groups"
 scalacOptions in (Compile, doc) += "-diagrams"
 scalacOptions in (Compile, doc) += "-diagrams-debug"
 
-lazy val commonSettings = Seq (
-  organization     := "com.workflowfm",
-  scalaVersion     := "2.12.10"
+lazy val commonSettings = Seq(
+  organization := "com.workflowfm",
+  scalaVersion := "2.12.10"
 )
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.0-SNAP10" % Test
-libraryDependencies +=  "org.scalacheck" %% "scalacheck" % "1.14.0" % Test
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.0" % Test
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.4.12"
 libraryDependencies += "com.typesafe.akka" %% "akka-testkit" % "2.5.16" % "test"
 
-libraryDependencies += "com.typesafe.akka" %% "akka-stream"       % "2.5.13"
+libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.13"
 
 libraryDependencies += "uk.ac.ed.inf" %% "subakka" % "0.1-SNAPSHOT"
-libraryDependencies += "uk.ac.ed.inf" %% "subakka" % "0.1-SNAPSHOT" % Test  classifier "tests"
+libraryDependencies += "uk.ac.ed.inf" %% "subakka" % "0.1-SNAPSHOT" % Test classifier "tests"
 
 libraryDependencies += "org.apache.commons" % "commons-lang3" % "3.3.2"
 
 libraryDependencies += "junit" % "junit" % "4.8.2"
 
-
 lazy val root = (project in file("."))
   .settings(
     commonSettings,
     name := "wfm-simulator"
-)
+  )
 
 lazy val flowsExample = project
   .in(file("flowsExample"))
   .settings(
     commonSettings,
     libraryDependencies += "com.workflowfm" %% "wfm-simulator" % "0.3-SNAPSHOT",
-
     scalaSource in Compile := baseDirectory.value / "src",
     scalaSource in Test := baseDirectory.value / "test"
-)
+  )
--- src/main/scala/com/workflowfm/simulator/flows/Flow.scala
+++ src/main/scala/com/workflowfm/simulator/flows/Flow.scala
@@ -5,20 +5,19 @@ import scala.concurrent.{ ExecutionContext, Future, Promise }
 import akka.actor.{ Actor, ActorRef, Props }
 import java.util.UUID
 
-
 sealed trait Flow {
-    val id: UUID = java.util.UUID.randomUUID
-    def +(f:Flow) = And(this,f)
-    def >(f:Flow) = Then(this,f)
-    def |(f:Flow) = Or(this,f)
+  val id: UUID = java.util.UUID.randomUUID
+  def +(f: Flow) = And(this, f)
+  def >(f: Flow) = Then(this, f)
+  def |(f: Flow) = Or(this, f)
 }
 
 case class NoTask() extends Flow
-case class FlowTask(generator:TaskGenerator, resources:Seq[String]) extends Flow
-case class Then(left:Flow,right:Flow) extends Flow
-case class And(left:Flow,right:Flow) extends Flow
-case class All(elements:Flow*) extends Flow
-case class Or(left:Flow,right:Flow) extends Flow
+case class FlowTask(generator: TaskGenerator, resources: Seq[String]) extends Flow
+case class Then(left: Flow, right: Flow) extends Flow
+case class And(left: Flow, right: Flow) extends Flow
+case class All(elements: Flow*) extends Flow
+case class Or(left: Flow, right: Flow) extends Flow
 
 /**
   * An actor which implements the "Flows" interface which can be used to create simple workflow simulations.
@@ -26,110 +25,111 @@ case class Or(left:Flow,right:Flow) extends Flow
   * This actor uses a [[Flow]] structure which describes how and in what order certain tasks should be executed.
   * A Flow may consist of a single FlowTask or some combination of FlowTasks which are joined using `And`s, `Then`s,
   * and `Or`s.
-  * 
+  *
   * @param name The name of the simulation being managed.
   * @param coordinator A reference to the [[Coordinator]] actor running the simulation.
   * @param flow The flow which describes how the simulation behaves
   * @param executionContext
   */
-class FlowSimulationActor (
+class FlowSimulationActor(
     name: String,
     coordinator: ActorRef,
     flow: Flow
-) (implicit executionContext: ExecutionContext)
-        extends AsyncSimulation(name, coordinator)(executionContext) {
+)(implicit executionContext: ExecutionContext)
+    extends AsyncSimulation(name, coordinator)(executionContext) {
 
-    /**
-      * Initiates the execution of the simulation.
-      *
-      * @return A `Future` that completes with a custom output when the simulation is completed.
-      */
-    override def run(): Future[Any] = {
-        val promise = Promise[Any]()
-        runFlow(flow, ( (_,_)=>promise.success(Unit) ) )
-        ready()
-        promise.future
+  /**
+    * Initiates the execution of the simulation.
+    *
+    * @return A `Future` that completes with a custom output when the simulation is completed.
+    */
+  override def run(): Future[Any] = {
+    val promise = Promise[Any]()
+    runFlow(flow, ((_, _) => promise.success(Unit)))
+    ready()
+    promise.future
+  }
+
+  /**
+    * Runs a `Flow` by either innitiating a FlowTask or decomposing a more complex flow via the `execute` method.
+    * In both cases, the callback of the flow is stored in a map.
+    *
+    * @param flow The flow to run.
+    * @param callback The callback function which is executed once this flow completes.
+    */
+  protected def runFlow(flow: Flow, callback: Callback): Unit = {
+    flow match {
+      case f: FlowTask =>
+        task(f.id, f.generator, ((t, l) => { callback(t, l); ack(Seq(f.id)) }), f.resources)
+      case f: Flow => { tasks += flow.id -> callback; execute(f) }
     }
+  }
 
-    /**
-      * Runs a `Flow` by either innitiating a FlowTask or decomposing a more complex flow via the `execute` method.
-      * In both cases, the callback of the flow is stored in a map.
-      *
-      * @param flow The flow to run.
-      * @param callback The callback function which is executed once this flow completes.
-      */
-    protected def runFlow(flow:Flow, callback:Callback):Unit = {
-        flow match {
-            case f: FlowTask => task(f.id, f.generator, ( (t,l)=>{callback(t,l); ack(Seq(f.id)) } ), f.resources)
-            case f: Flow => { tasks += flow.id -> callback; execute(f) }
-        }      
+  /**
+    * Completes an id by executing its callback and then removing it from the map
+    *
+    * This overloads the definition of `complete` found in [[Simulation]] to allow for any id to be used in the
+    * `tasks` map, and not just [[Task]] objects.
+    *
+    * @param id The id to complete
+    */
+  protected def complete(id: UUID) = {
+    tasks.get(id).map(_(null, 0L))
+    tasks -= id
+  }
+
+  /**
+    * Executes a flow by translating from a flow object to its sub-parts and appropriate
+    * callbacks, then calling `runFlow`.
+    *
+    * @param flow The flow to be executed
+    */
+  protected def execute(flow: Flow) {
+    flow match {
+      case f: NoTask => complete(f.id)
+
+      case FlowTask(generator: TaskGenerator, resources: Seq[String]) => {}
+      //this is here for the sake of case completeness, should not be called
+
+      case f: Then => {
+        val rightCallback: Callback = (_, _) => complete(f.id)
+        val leftCallback: Callback = (_, _) => runFlow(f.right, rightCallback)
+        runFlow(f.left, leftCallback)
+      }
+
+      case f: And => {
+        val leftCallback: Callback = (_, _) => (if (!tasks.contains(f.right.id)) complete(f.id))
+        val rightCallback: Callback = (_, _) => (if (!tasks.contains(f.left.id)) complete(f.id))
+        runFlow(f.left, leftCallback)
+        runFlow(f.right, rightCallback)
+      }
+
+      case f @ All(elem @ _*) =>
+        runFlow((elem.fold(NoTask()) { (l, r) => And(l, r) }), (_, _) => complete(f.id))
+
+      case f: Or => {
+        val leftCallback: Callback = (_, _) => (if (tasks.contains(f.right.id)) complete(f.id))
+        val rightCallback: Callback = (_, _) => (if (tasks.contains(f.left.id)) complete(f.id))
+        runFlow(f.left, leftCallback)
+        runFlow(f.right, rightCallback)
+      }
     }
-
-    /**
-      * Completes an id by executing its callback and then removing it from the map
-      * 
-      * This overloads the definition of `complete` found in [[Simulation]] to allow for any id to be used in the
-      * `tasks` map, and not just [[Task]] objects.
-      * 
-      * @param id The id to complete
-      */
-    protected def complete(id: UUID) = {
-        tasks.get(id).map (_(null,0L))
-        tasks -= id
-    }
-
-    /**
-      * Executes a flow by translating from a flow object to its sub-parts and appropriate 
-      * callbacks, then calling `runFlow`. 
-      *
-      * @param flow The flow to be executed
-      */
-    protected def execute(flow:Flow) {
-        flow match {
-            case f:NoTask => complete(f.id)
-
-            case FlowTask(generator:TaskGenerator, resources:Seq[String]) => {} 
-            //this is here for the sake of case completeness, should not be called
-
-            case f:Then => {
-                val rightCallback: Callback = (_,_) => complete(f.id)
-                val leftCallback: Callback = (_,_) => runFlow(f.right, rightCallback)
-                runFlow(f.left, leftCallback)
-            }
-
-            case f:And => {
-                val leftCallback: Callback = (_,_) => ( if (!tasks.contains(f.right.id)) complete(f.id) )
-                val rightCallback: Callback = (_,_) => ( if (!tasks.contains(f.left.id)) complete(f.id) )
-                runFlow(f.left,leftCallback)
-                runFlow(f.right,rightCallback)
-            }
-
-            case f @ All(elem@_*) => runFlow( (elem.fold(NoTask()) { (l,r)=>And(l,r) }), (_,_)=>complete(f.id) )
-
-            case f:Or => {
-                val leftCallback: Callback = (_,_) => ( if (tasks.contains(f.right.id)) complete(f.id) )
-                val rightCallback: Callback = (_,_) => ( if (tasks.contains(f.left.id)) complete(f.id) )
-                runFlow(f.left,leftCallback)
-                runFlow(f.right,rightCallback)
-            }
-        }
-    }
+  }
 }
 
 object FlowSimulationActor {
-    /**
-      * Creates props of a [[FlowSimulationActor]].
-      *
-      * @param name The simulation name.
-      * @param coordinator The [[Coordinator]].
-      * @param flow The [[Flow]] to be executed
-      * @param executionContext
-      * @return The Props of a new flow simulation actor
-      */
-    def props (
-        name: String,
-        coordinator: ActorRef,
-        flow: Flow)
-    (implicit executionContext: ExecutionContext): Props =
-        Props(new FlowSimulationActor(name, coordinator, flow))
+
+  /**
+    * Creates props of a [[FlowSimulationActor]].
+    *
+    * @param name The simulation name.
+    * @param coordinator The [[Coordinator]].
+    * @param flow The [[Flow]] to be executed
+    * @param executionContext
+    * @return The Props of a new flow simulation actor
+    */
+  def props(name: String, coordinator: ActorRef, flow: Flow)(
+      implicit executionContext: ExecutionContext
+  ): Props =
+    Props(new FlowSimulationActor(name, coordinator, flow))
 }
