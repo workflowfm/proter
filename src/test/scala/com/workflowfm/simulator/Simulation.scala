4a41ba994ed5ee7092af71e764805c5f2b52840f
MBaczun <s1743497@ed.ac.uk>
2020-08-26 11:50:51 +0100

    updating tests

--- src/test/scala/com/workflowfm/simulator/Simulation.scala
+++ src/test/scala/com/workflowfm/simulator/Simulation.scala
@@ -71,15 +71,15 @@ class SimulationTests
                     val id1 = java.util.UUID.randomUUID
                     val id2 = java.util.UUID.randomUUID
                     val id3 = java.util.UUID.randomUUID
-                    val task1 = futureTask(id1, TaskGenerator("task1","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"):_*)
+                    val task1 = futureTask(id1, TaskGenerator("task1","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"))
                     ready()
                     val task2 = task1 flatMap { _=> 
-                        val t = futureTask(id2,TaskGenerator("task2","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"):_*)
+                        val t = futureTask(id2,TaskGenerator("task2","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"))
                         ack(Seq(id1))
                         t
                     }
                     val task3 = task2 flatMap { _=> 
-                        val t = futureTask(id3,TaskGenerator("task3","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"):_*)
+                        val t = futureTask(id3,TaskGenerator("task3","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"))
                         ack(Seq(id2))
                         t
                     }
@@ -119,53 +119,53 @@ class SimulationTests
 
         }
         
-        "add tasks in the future" in {
-            class DummySim(name: String, coordinator: ActorRef)
-            (implicit executionContext: ExecutionContext) 
-            extends Simulation(name,coordinator) {
-                val promise = Promise[Any]()
-                override def run():Future[Any] = { 
-                    val id1 = java.util.UUID.randomUUID
-                    val id2 = java.util.UUID.randomUUID
-                    val id3 = java.util.UUID.randomUUID
-                    val task1 = task(id1, TaskGenerator("task1","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"),0L)
-                    val task2 = task(id2,TaskGenerator("task2","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"),0L,Seq(id1)) 
-                    val task3 = task(id3,TaskGenerator("task3","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"),0L,Seq(id2))
-                    ready()
-                    promise.future
-                } 
+        // "add tasks in the future" in {
+        //     class DummySim(name: String, coordinator: ActorRef)
+        //     (implicit executionContext: ExecutionContext) 
+        //     extends Simulation(name,coordinator) {
+        //         val promise = Promise[Any]()
+        //         override def run():Future[Any] = { 
+        //             val id1 = java.util.UUID.randomUUID
+        //             val id2 = java.util.UUID.randomUUID
+        //             val id3 = java.util.UUID.randomUUID
+        //             val task1 = task(id1, TaskGenerator("task1","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"),0L)
+        //             val task2 = task(id2,TaskGenerator("task2","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"),0L,Seq(id1)) 
+        //             val task3 = task(id3,TaskGenerator("task3","sim",ConstantGenerator(2L),ConstantGenerator(0L)),Seq("r1"),0L,Seq(id2))
+        //             ready()
+        //             promise.future
+        //         } 
 
-                override def complete(task: Task, time: Long): Unit = {
-                    ack(Seq(task.id))
-                    if (task.name=="task3") promise.success(Unit)
-                }
-            }
+        //         override def complete(task: Task, time: Long): Unit = {
+        //             ack(Seq(task.id))
+        //             if (task.name=="task3") promise.success(Unit)
+        //         }
+        //     }
 
-            val sim = system.actorOf(Props(new DummySim("sim",self)))
+        //     val sim = system.actorOf(Props(new DummySim("sim",self)))
 
-            sim ! Simulation.Start
-            expectMsg( Coordinator.SimStarted("sim"))
-            val Coordinator.AddTaskInFuture(id1, generator1, resources1, time1, prerequisites1) = expectMsgType[ Coordinator.AddTaskInFuture ]
-            val Coordinator.AddTaskInFuture(id2, generator2, resources2, time2, prerequisites2) = expectMsgType[ Coordinator.AddTaskInFuture ]
-            val Coordinator.AddTaskInFuture(id3, generator3, resources3, time3, prerequisites3) = expectMsgType[ Coordinator.AddTaskInFuture ]
-            expectMsg( Coordinator.SimReady )
+        //     sim ! Simulation.Start
+        //     expectMsg( Coordinator.SimStarted("sim"))
+        //     val Coordinator.AddTaskInFuture(id1, generator1, resources1, time1, prerequisites1) = expectMsgType[ Coordinator.AddTaskInFuture ]
+        //     val Coordinator.AddTaskInFuture(id2, generator2, resources2, time2, prerequisites2) = expectMsgType[ Coordinator.AddTaskInFuture ]
+        //     val Coordinator.AddTaskInFuture(id3, generator3, resources3, time3, prerequisites3) = expectMsgType[ Coordinator.AddTaskInFuture ]
+        //     expectMsg( Coordinator.SimReady )
 
-            val task1 = generator1.create(id1,0L,sim,"r1")
-            val task2 = generator2.create(id2,0L,sim,"r1")
-            val task3 = generator3.create(id3,0L,sim,"r1")
+        //     val task1 = generator1.create(id1,0L,sim,"r1")
+        //     val task2 = generator2.create(id2,0L,sim,"r1")
+        //     val task3 = generator3.create(id3,0L,sim,"r1")
 
-            sim ! Simulation.TaskCompleted(task1,2L)
-            expectMsg( Coordinator.AckTasks(Seq(id1)))
+        //     sim ! Simulation.TaskCompleted(task1,2L)
+        //     expectMsg( Coordinator.AckTasks(Seq(id1)))
 
-            sim ! Simulation.TaskCompleted(task2,4L)
-            expectMsg( Coordinator.AckTasks(Seq(id2)))
+        //     sim ! Simulation.TaskCompleted(task2,4L)
+        //     expectMsg( Coordinator.AckTasks(Seq(id2)))
 
-            sim ! Simulation.TaskCompleted(task3,6L)
-            expectMsg( Coordinator.AckTasks(Seq(id3)))
-            val Coordinator.SimDone(name, future) = expectMsgType[ Coordinator.SimDone ]
-            name should be ("sim")
-            //expectNoMsg()
-        }
+        //     sim ! Simulation.TaskCompleted(task3,6L)
+        //     expectMsg( Coordinator.AckTasks(Seq(id3)))
+        //     val Coordinator.SimDone(name, future) = expectMsgType[ Coordinator.SimDone ]
+        //     name should be ("sim")
+        //     //expectNoMsg()
+        // }
 
         "use the lookahead trait to maintain a map of lookahead replies" in {
             class DummySim(name: String, coordinator: ActorRef)
@@ -188,9 +188,9 @@ class SimulationTests
                         {(_,_)=> task(id2,generator2,
                             {(_,_)=> task(id3,generator3, 
                                 {(_,_)=> promise.success(Unit)}, 
-                            resources, None, Seq()); ack(Seq(id2)) }, 
-                        resources, None, Seq()); ack(Seq(id1)) }, 
-                    resources, None, Seq())
+                            resources); ack(Seq(id2)) }, 
+                        resources); ack(Seq(id1)) }, 
+                    resources)
 
                     ready()
                     promise.future
