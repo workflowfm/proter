7cd6d3f28860a799f0cb9a93d5373d2285929e10
MBaczun <s1743497@ed.ac.uk>
2020-12-18 10:29:50 +0000

    WIP working with JBPM to get tasks.
    Trying for months now to get the tasks from a bpmn file in order such
    that we can simulate them. This commit contains some essentials for
    running the jBPM library and some test bpmn files in the resources
    folder. The build file has been edited to include all the necessary
    dependencies (and there are a lot of them).
    
    As of writing it is my understanding that working with abstract tasks
    and script tasks is not possible. Human tasks are the last hope, but
    they have proven difficult to work with due to requiring users which are
    members of various groups. I have yet to make even one working human
    model.

---
+++ src/main/scala/com/workflowfm/simulator/BPMN.scala
@@ -0,0 +1,113 @@
+package com.workflowfm.simulator
+
+import com.workflowfm.simulator._
+import scala.concurrent.{ ExecutionContext, Future, Promise }
+import akka.actor.{ActorRef, Props}
+
+import org.kie.api.KieBase
+
+import scala.collection.mutable.Map;
+import collection.JavaConverters._
+
+import java.io._
+
+import org.kie.api.KieServices;
+import org.kie.api.io.ResourceType;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeEnvironment;
+import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.manager.RuntimeManagerFactory;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.TaskSummary;
+import org.kie.test.util.db.PersistenceUtil;
+import scala.concurrent.Future
+
+import scala.io.StdIn.readLine
+
+object BPMN {
+
+    def runScriptProcess(path: String, name: String): Unit = {
+        //val manager: RuntimeManager = createManager(path)
+        val manager: RuntimeManager = getRuntimeManager(path)
+        val runtime: RuntimeEngine = manager.getRuntimeEngine(null)
+        val ksession: KieSession = runtime.getKieSession()
+
+        val params: Map[String, Object] = Map[String, Object]()
+        params.put("employee", "krisv");
+        params.put("reason", "Yearly performance evaluation");
+
+        try { 
+            ksession.startProcess(name, params.asJava);
+
+            val taskService: TaskService = runtime.getTaskService();
+            val tasks: List[TaskSummary] = taskService.getTasksAssignedAsPotentialOwner("krisv", "en-UK").asScala.toList
+            val tasks2: List[TaskSummary] = taskService.getTasksAssignedAsBusinessAdministrator("wbadmin", "en-UK").asScala.toList
+            //val task = tasks.head
+            println("tasks: "+ tasks.length)
+            println("tasks2: "+ tasks2.length)
+            //System.out.println("'testName' completing task " + task.getName() + ": " + task.getDescription());
+
+        } catch {
+            case wfre: Throwable => wfre.printStackTrace(); //WorkflowRuntimeException
+        }
+        
+        manager.close();
+    }
+  
+    ////////// human actor version
+    def getRuntimeManager(process: String): RuntimeManager = {
+        PersistenceUtil.setupPoolingDataSource();
+        val environment: RuntimeEnvironment = RuntimeEnvironmentBuilder.Factory.get().newDefaultBuilder().addAsset(KieServices.Factory.get().getResources().newClassPathResource(process), ResourceType.BPMN2).get();
+        return RuntimeManagerFactory.Factory.get().newSingletonRuntimeManager(environment);
+    } /////////
+
+    def createManager(process: String): RuntimeManager = {
+    	val environment: RuntimeEnvironment = RuntimeEnvironmentBuilder.Factory.get().newEmptyBuilder()
+            .addAsset(KieServices.Factory.get().getResources()
+        		.newClassPathResource(process), ResourceType.BPMN2)
+            .get();
+        return RuntimeManagerFactory.Factory.get().newSingletonRuntimeManager(environment);
+    }
+
+    def sayHello(): Unit = {
+        System.out.println("hello!")
+        var t = readLine()
+        Thread.sleep(2000)
+        println("got: "+t)
+    }
+    
+    def sayGoodbye(): Unit = {
+        System.out.println("goodbye!");
+    }
+
+}
+
+class BPMN() {
+
+
+}
+
+class BPMNSimulationActor(
+    name: String,
+    coordinator: ActorRef,
+    process: String,
+    processName: String
+)(implicit executionContext: ExecutionContext)
+    extends AsyncSimulation(name, coordinator)(executionContext) {
+
+    override def run(): Future[Any] = {
+        BPMN.runScriptProcess(process, processName)
+        Future.unit
+        
+    }
+
+}
+
+object BPMNSimulationActor {
+    def props(name: String, coordinator: ActorRef, process: String, processName: String)(
+        implicit executionContext: ExecutionContext
+    ): Props =
+    Props(new BPMNSimulationActor(name, coordinator, process, processName))
+}
---
+++ src/main/scala/com/workflowfm/simulator/BPMNMain.scala
@@ -0,0 +1,79 @@
+package com.workflowfm.simulator.flowsExample
+
+import akka.actor.ActorSystem
+import akka.util.Timeout
+import akka.pattern.ask
+import scala.concurrent._
+import scala.concurrent.duration._
+import com.workflowfm.simulator._
+import com.workflowfm.simulator.metrics._
+import com.workflowfm.simulator.events.{ ShutdownHandler }
+import com.workflowfm.simulator.flows._
+import uk.ac.ed.inf.ppapapan.subakka.Subscriber
+import java.io.File
+
+object BPMNMain {
+    //toggle for debug
+    val DEBUG = false
+
+    def main(args: Array[String]): Unit = {
+
+        implicit val system: ActorSystem = ActorSystem("FlowsMain")
+        implicit val executionContext: ExecutionContext = ExecutionContext.global
+        implicit val timeout = Timeout(2.seconds)
+
+        val coordinator = system.actorOf(Coordinator.props(new DefaultScheduler()))
+        val shutdownActor = Subscriber.actor(new ShutdownHandler())
+
+        val handler = SimMetricsOutputs(
+	        new SimMetricsPrinter(),
+            new SimCSVFileOutput("flowsExample" + File.separator + "output" + File.separator,"FlowsMain"),
+	        new SimD3Timeline("flowsExample" + File.separator + "output" + File.separator,"FlowsMain")
+        )
+
+        Await.result(new SimOutputHandler(handler).subAndForgetTo(coordinator,Some("MetricsHandler")), 3.seconds)
+        Await.result(shutdownActor ? Subscriber.SubAndForgetTo(coordinator), 3.seconds)
+
+        if (DEBUG) {
+            println(s"Cores: ${Runtime.getRuntime().availableProcessors()}")
+            val config = system.settings.config.getConfig("akka.actor.default-dispatcher")
+            println(s"Parallelism: ${config.getInt("fork-join-executor.parallelism-min")}-${config.getInt("fork-join-executor.parallelism-max")} x ${config.getDouble("fork-join-executor.parallelism-factor")}")
+            val printer = new com.workflowfm.simulator.events.PrintEventHandler
+            Await.result(printer.subAndForgetTo(coordinator), 3.seconds)
+        }
+
+        //=========================================================================================
+
+        // // Define resources
+        // val r1 = new TaskResource("r1",0)
+        // val r2 = new TaskResource("r2",0)
+        // val r3 = new TaskResource("r3",0)
+        // val r4 = new TaskResource("r4",0)
+        // val r5 = new TaskResource("r5",0)
+        // val r6 = new TaskResource("r6",0)
+        // val r7 = new TaskResource("r7",0)
+        // val r8 = new TaskResource("r8",0)
+        // val resources = List (r1,r2,r3,r4,r5,r6,r7,r8)
+        // coordinator ! Coordinator.AddResources(resources)
+
+        // // Define tasks 
+        // val task1 = FlowTask(TaskGenerator("task1","sim1",ConstantGenerator(1L),ConstantGenerator(0L)).withResources(Seq("r1")))
+        // val task2 = FlowTask(TaskGenerator("task2","sim1",ConstantGenerator(1L),ConstantGenerator(0L)).withResources(Seq("r2")))
+        // val task3 = FlowTask(TaskGenerator("task3","sim1",ConstantGenerator(1L),ConstantGenerator(0L)).withResources(Seq("r3")))
+
+        // val flow = task1 > task2 > task3
+        
+        //BPMN.runScriptProcess("myBpmn.bpmn", "MyTest.process")
+
+        //coordinator ! Coordinator.AddSim(0L,system.actorOf(FlowSimulationActor.props("sim1",coordinator,flow),"sim1"))
+                                                                                                    // "myBpmn.bpmn", "MyTest.process"
+                                                                                                    // "Evaluation.bpmn", "com.sample.evaluation"
+        coordinator ! Coordinator.AddSim(0L,system.actorOf(BPMNSimulationActor.props("sim1",coordinator,"Evaluation.bpmn", "com.sample.evaluation"),"sim1"))
+        //coordinator ! Coordinator.AddSim(0L,system.actorOf(BPMNSimulationActor.props("sim1",coordinator,"myBpmn.bpmn", "MyTest.process"),"sim1"))
+        //coordinator ! Coordinator.AddSim(0L,system.actorOf(BPMNSimulationActor.props("sim1",coordinator,"test_human2.bpmn", "MyTest.test_human"),"sim1"))
+       // coordinator ! Coordinator.AddSim(0L,system.actorOf(BPMNSimulationActor.props("sim1",coordinator,"test_script.bpmn", "MyTest.test_script"),"sim1"))
+
+        coordinator ! Coordinator.Start
+    }
+
+}
