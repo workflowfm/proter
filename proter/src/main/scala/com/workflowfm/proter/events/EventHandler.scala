6df98d7e6a6515d025863cf13dbf015acc801270
Petros Papapanagiotou <petrospapapan@gmail.com>
2021-03-16 22:56:54 +0000

    Updates scaladoc for `EventHandler`s

--- proter/src/main/scala/com/workflowfm/proter/events/EventHandler.scala
+++ proter/src/main/scala/com/workflowfm/proter/events/EventHandler.scala
@@ -7,14 +7,45 @@ import scala.collection.mutable.HashSet
 import scala.concurrent.Promise
 
 /**
-  * A [[uk.ac.ed.inf.ppapapan.subakka.Subscriber]] for [[Coordinator]] [[Event]]s.
+  * A handler of [[Event]]s.
+  * 
+  * Handles a stream of events from a [[Publisher]].
   */
 trait EventHandler {
+  /**
+    * A unique identifier for the handler.
+    *
+    * @return a unique identifier
+    */
   val id: UUID = UUID.randomUUID
 
+  /**
+    * Handles the initialisation of a new event stream.
+    *
+    * @param publisher The [[Publisher]] that started the stream.
+    */
   def onInit(publisher: Publisher): Unit = ()
+
+  /**
+    * Handles an [[Event]] in the stream.
+    *
+    * @param event The [[Event]] to handle.
+    */
   def onEvent(event: Event): Unit = ()
+
+  /**
+    * Handles the end of a stream.
+    *
+    * @param publisher The [[Publisher]] that ended the stream.
+    */
   def onDone(publisher: Publisher): Unit = ()
+
+  /**
+    * Handles an error in the stream.
+    *
+    * @param e The throwable error that occurred.
+    * @param publisher The [[Publisher]] that threw the error.
+    */
   def onFail(e: Throwable, publisher: Publisher): Unit = ()
 }
 
@@ -24,34 +55,35 @@ trait EventHandler {
 trait PoolEventHandler extends EventHandler {
 
   /**
-    * The set of [[Coordinator]]s we have subscribed to.
+    * The set of [[Publisher]]s we have subscribed to.
     */
-  val coordinators: HashSet[Publisher] = HashSet[Publisher]()
+  val publishers: HashSet[Publisher] = HashSet[Publisher]()
 
   /**
-    * Initializes the event stream by adding the coordinator to [[coordinators]].
-    *
-    * @param a The reference to the [[Coordinator]] actor that initialized the stream.
+    * @inheritdoc
+    * 
+    * Adds the publisher to [[publishers]].
     */
-  override def onInit(c: Publisher): Unit = {
-    //log.debug(s"Pool handler adding coordinator: $a")
-    coordinators += c
+  override def onInit(publisher: Publisher): Unit = {
+    publishers += publisher
   }
 
   /**
-    * Closes the event stream by removing the coordinator from [[coordinators]].
-    *
-    * @param a The reference to the [[Coordinator]] that closed the stream.
-    * @param s
+    * @inheritdoc
+    * 
+    * Closes the event stream by removing the publisher from [[publishers]].
     */
-  override def onDone(c: Publisher): Unit = {
-    //log.debug(s"Pool handler done with coordinator: $a")
-    coordinators -= c
+  override def onDone(publisher: Publisher): Unit = {
+    publishers -= publisher
   }
 
-  override def onFail(e: Throwable, c: Publisher): Unit = {
-    //log.debug(s"Pool handler done with coordinator: $a")
-    coordinators -= c
+  /**
+    * @inheritdoc
+    *
+    * Closes the event stream by removing the publisher from [[publishers]].
+    */
+  override def onFail(e: Throwable, publisher: Publisher): Unit = {
+    publishers -= publisher
   }
 }
 
@@ -59,6 +91,9 @@ trait PoolEventHandler extends EventHandler {
   * An [[EventHandler]] that prints events to standard error.
   */
 class PrintEventHandler extends EventHandler {
+  /**
+    * A simple date formatter for printing the current (system) time.
+    */
   val formatter = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS")
 
   override def onEvent(e: Event): Unit = {
@@ -72,6 +107,11 @@ class PrintEventHandler extends EventHandler {
   * @tparam R The type of the result.
   */
 trait ResultHandler[R] extends EventHandler {
+  /**
+    * The result of the handler.
+    *
+    * @return The result of the handler.
+    */
   def result: R
 }
 
@@ -79,64 +119,153 @@ trait ResultHandler[R] extends EventHandler {
   * A [[ResultHandler]] that counts the events seen.
   */
 class CounterHandler extends ResultHandler[Int] {
+  /**
+    * The counter of handled events.
+    */
   var count = 0
 
-  override def onInit(c: Publisher): Unit = count = 0
+  /**
+    * @inheritdoc
+    * 
+    * Resets the counter to 0.
+    */
+  override def onInit(publisher: Publisher): Unit = count = 0
+
+  /**
+    * @inheritdoc
+    * 
+    * Increases the counter by one.
+    */
   override def onEvent(e: Event): Unit = count = count + 1
+
+  /**
+    * @inheritdoc
+    * 
+    * Simply returns the counter.
+    */
   override def result = count
 }
 
 /**
-  * A wrapper for a [[ResultHandler]] that fulfills a [[scala.concurrent.Promise]] with the result.
+  * A wrapper for a [[ResultHandler]] that fulfills a [[scala.concurrent.Promise Promise]] with the result.
+  * 
   * This is also itself a [[ResultHandler]].
   *
   * @param handler The [[ResultHandler]] that calculates the result.
   */
 class PromiseHandler[R](handler: ResultHandler[R]) extends ResultHandler[R] {
+  /**
+    * The promise to be fulfilled with the result.
+    */
   protected val promise: Promise[R] = Promise[R]()
+
+  /**
+    * Returns a [[scala.concurrent.Future Future]] of the result.
+    *
+    * @return A future result.
+    */
   def future = promise.future
 
-  override def onInit(c: Publisher): Unit = handler.onInit(c)
+  /**
+    * @inheritdoc
+    *
+    * Delegates this to the result handler.
+    */
+  override def onInit(publisher: Publisher): Unit = handler.onInit(publisher)
+
+  /**
+    * @inheritdoc
+    *
+    * Delegates this to the result handler.
+    */
   override def onEvent(e: Event): Unit = handler.onEvent(e)
 
-  override def onDone(c: Publisher): Unit = {
-    handler.onDone(c)
+  /**
+    * @inheritdoc
+    *
+    * Delegates this to the result handler and then fulfills the promise.
+    */
+  override def onDone(publisher: Publisher): Unit = {
+    handler.onDone(publisher)
     if (!promise.isCompleted) promise.success(result)
   }
 
-  override def onFail(ex: Throwable, c: Publisher): Unit = {
-    handler.onFail(ex, c)
+  /**
+    * @inheritdoc
+    *
+    * Delegates this to the result handler and then fulfills the promise with a failure.
+    */
+  override def onFail(ex: Throwable, publisher: Publisher): Unit = {
+    handler.onFail(ex, publisher)
     if (!promise.isCompleted) promise.failure(ex)
   }
 
+  /**
+    * @inheritdoc
+    *
+    * Delegates this to the result handler.
+    */
   override def result = handler.result
 }
 
 object PromiseHandler {
+  /**
+    * Declarative constructor for a [[PromiseHandler]] given a [[ResultHandler]].
+    *
+    * @param handler The [[ResultHandler]] to use.
+    * @return The constructed [[PromiseHandler]].
+    */
   def of[R](handler: ResultHandler[R]): PromiseHandler[R] = new PromiseHandler[R](handler)
 }
 
 /**
   * Listens for the end of a named simulation and handles its result.
   *
-  * @param name The name of the [[Simulation]] to listen for.
+  * @param name The name of the [[SimulationRef]] to listen for.
   * @param callback A function to handle the results of the simulation when it completes.
   */
 class SimulationResultHandler(name: String, callback: String => Unit = { _ => Unit })
     extends ResultHandler[Option[String]] {
-  var coordinator: Option[Publisher] = None
+
+  /**
+    * The [[Publisher]] of the stream.
+    * 
+    * We assume to be subscribed to a single publisher. We then keep track of it so we can unsubscribe
+    * as soon as we get our result.
+    */
+  var publisher: Option[Publisher] = None
+
+  /**
+    * The result of the simulation when we get it.
+    */
   var simResult: Option[String] = None
 
-  override def onInit(publisher: Publisher): Unit = coordinator = Some(publisher)
+  /**
+    * @inheritdoc
+    *
+    * Sets the [[publisher]].
+    */
+  override def onInit(publisher: Publisher): Unit = this.publisher = Some(publisher)
 
+  /**
+    * @inheritdoc
+    *
+    * If the event is [[ESimEnd]] and the simulation name matches then 
+    * we record the simulation result in [[simResult]], unsubscribe, and call the callback function.
+    */
   override def onEvent(evt: Event): Unit = evt match {
     case ESimEnd(_, _, n, r) if n == name => {
-      coordinator.map(_.unsubscribe(this))
+      publisher.map(_.unsubscribe(this))
       simResult = Some(r)
       callback(r)
     }
     case _ => Unit
   }
 
+  /**
+    * @inheritdoc
+    *
+    * @return The result of the simulation or `None` if we have not received one (yet).
+    */
   override def result = simResult
 }
