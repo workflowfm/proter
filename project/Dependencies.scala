b81850d85764ebb76915893c8ad3528315a3e59e
Petros Papapanagiotou <petrospapapan@gmail.com>
2021-10-13 15:37:00 +0100

    Updates project structure and dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,15 +11,12 @@ object Dependencies {
   val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0" % "test"
   val junit = "junit" % "junit" % "4.8.2" % "test"
 
-  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.6.1"
-  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.6.1" % "test"
-
-  val apache = "org.apache.commons" % "commons-lang3" % "3.3.2"
+  val proter = "com.workflowfm" %% "proter" % "0.7.3"
 
   val sortImports = "com.nequissimus" %% "sort-imports" % "0.5.4"
 
   val common: Seq[ModuleID] = Seq(
-    apache
+    proter
   )
 
   val testAll: Seq[ModuleID] = Seq(
@@ -29,11 +26,6 @@ object Dependencies {
     junit,
   )
 
-  val akka: Seq[ModuleID] = Seq (
-    akkaActor,
-    akkaTestkit,
-    akkaTestkit,
-  )
 }
 
 
