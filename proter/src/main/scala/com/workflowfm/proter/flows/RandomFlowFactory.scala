21614f6cc1a621e1df2058a0594799dbcccbe727
Petros Papapanagiotou <petrospapapan@gmail.com>
2021-12-02 11:22:11 +0000

    Changes `RandomFlowFactory` distributions to use `_.getLong`
    This is more accurate than `Math.round(_.get)`.
    
    Also renamed `duration` to `durations`.

--- proter/src/main/scala/com/workflowfm/proter/flows/RandomFlowFactory.scala
+++ proter/src/main/scala/com/workflowfm/proter/flows/RandomFlowFactory.scala
@@ -8,7 +8,7 @@ import com.workflowfm.proter._
     * @param andProbability Probability that a flow node is an [[And]]. Must be in range [0,1]. The inverse probability yeilds [[Then]] nodes.
     * @param resources The list of available resources
     * @param numTasks Distribution determining the number of tasks in the flow. Distribution values must not exceed `Int.MaxValue` due to truncation during rounding from Long to Int
-    * @param duration Distribution determining task duration. Distribution values must not exceed `Int.MaxValue` due to truncation during rounding from Long to Int
+    * @param durations Distribution determining task duration.
     * @param numResources Distribution determining number of resources required by each task. Distribution values must not exceed `Int.MaxValue` due to truncation during rounding from Long to Int
     * @param cost Distribution determining cost of a task
     * @param priority Distribution determining priority of a task
@@ -17,7 +17,7 @@ case class RandomFlowFactory(
     andProbability: Float, //balance between and and then nodes
     resources: Seq[TaskResource],
     numTasks: Distribution = Constant(1),
-    duration: Distribution = Constant(1),
+    durations: Distribution = Constant(1),
     numResources: Distribution = Constant(1),
     cost: Distribution = Constant(0),
     priority: Distribution = Uniform(-2,2)
@@ -25,12 +25,12 @@ case class RandomFlowFactory(
 
   def withResources(r: Seq[TaskResource]): RandomFlowFactory = copy(resources=r)
   def withTasks(d: Distribution): RandomFlowFactory = copy(numTasks=d)
-  def withDurations(d: Distribution): RandomFlowFactory = copy(duration=d)
+  def withDurations(d: Distribution): RandomFlowFactory = copy(durations=d)
   def withNumResources(d: Distribution): RandomFlowFactory = copy(numResources=d)
   def withRandomPriority(d: Distribution): RandomFlowFactory = copy(priority=d)
 
   def build(): Flow = {
-    val goal: Int = Math.round(numTasks.get).toInt
+    val goal: Int = numTasks.getLong.toInt
     randomNode(goal, 1)
   }
 
@@ -38,7 +38,7 @@ case class RandomFlowFactory(
     num match {
       case 1 => randomTask(name)
       case _ => {
-        val leftNum = Math.round(Uniform(1, num-1).get).toInt
+        val leftNum = Uniform(1, num-1).getLong.toInt
         if (new util.Random().nextDouble <= andProbability) {
           new And(randomNode(leftNum, name), randomNode(num-leftNum, name+leftNum))
         } else {
@@ -52,6 +52,6 @@ case class RandomFlowFactory(
     val numberOfResources = Math.round(numResources.get).toInt
     val possibleResources = resources.combinations(numberOfResources).toSeq
     val selectedResources = possibleResources(new util.Random().nextInt(possibleResources.length))
-    new FlowTask(Task("task"+name.toString,duration.getLong).withResources(selectedResources.map(_.name)).withCost(cost.get).withPriority(Math.round(priority.get).toInt))
+    new FlowTask(Task("task"+name.toString,durations.getLong).withResources(selectedResources.map(_.name)).withCost(cost.get).withPriority(priority.getLong.toInt))
   }
 }
