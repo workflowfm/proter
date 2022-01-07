e67e1a8c716d9d466f434e3aff7e617c2c10c8e4
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-01-07 14:21:29 +0000

    Updates scalamock to 5.2.0 and cleans dependencies up

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,9 +7,7 @@ object Dependencies {
   val scalaVer = "2.12.12"
 
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10" % "test"
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0" % "test"
-  val scalaMock = "org.scalamock" %% "scalamock" % "4.4.0" % "test"
-  val junit = "junit" % "junit" % "4.8.2" % "test"
+  val scalaMock = "org.scalamock" %% "scalamock" % "5.2.0" % "test"
 
   val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.6.1"
   val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.6.1" % "test"
@@ -24,14 +22,11 @@ object Dependencies {
 
   val testAll: Seq[ModuleID] = Seq(
     scalaTest,
-    scalaCheck,
     scalaMock,
-    junit,
   )
 
   val akka: Seq[ModuleID] = Seq (
     akkaActor,
-    akkaTestkit,
     akkaTestkit,
   )
 }
