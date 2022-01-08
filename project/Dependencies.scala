2fc15a3cdfc819a2dad741527c5f774658a98e90
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-01-08 23:31:48 +0000

    Bumps dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,12 +9,12 @@ object Dependencies {
   val scalaTest = "org.scalatest" %% "scalatest" % "3.2.10" % "test"
   val scalaMock = "org.scalamock" %% "scalamock" % "5.2.0" % "test"
 
-  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.6.1"
-  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.6.1" % "test"
+  val akkaActor = "com.typesafe.akka" %% "akka-actor" % "2.6.18"
+  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % "2.6.18" % "test"
 
   val apache = "org.apache.commons" % "commons-lang3" % "3.3.2"
 
-  val sortImports = "com.nequissimus" %% "sort-imports" % "0.5.4"
+  val sortImports = "com.nequissimus" %% "sort-imports" % "0.6.1"
 
   val common: Seq[ModuleID] = Seq(
     apache
