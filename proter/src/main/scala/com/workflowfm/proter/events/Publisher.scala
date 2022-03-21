eec0edf251c2f85061238168b1f650ce5d4fe16b
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-03-21 07:32:14 +0000

    Completes `Publisher` (at least for now)

--- proter/src/main/scala/com/workflowfm/proter/events/Publisher.scala
+++ proter/src/main/scala/com/workflowfm/proter/events/Publisher.scala
@@ -10,7 +10,7 @@ import java.util.UUID
 /**
   * A publisher of a stream of simulation [[Event]]s.
   */
-case class Publisher[F[_]](topic: Topic[F, Event], maxQueued: Int)(using MonadCancel[F, Throwable]) {
+case class Publisher[F[_]](topic: Topic[F, Either[Throwable, Event]], maxQueued: Int)(using MonadCancel[F, Throwable]) {
 
   /**
     * Publishes an event into the stream.
@@ -18,11 +18,16 @@ case class Publisher[F[_]](topic: Topic[F, Event], maxQueued: Int)(using MonadCa
     * @param evt
     *   The event to publish.
     */
-  def publish(evt: Event): F[Either[Topic.Closed, Unit]] = topic.publish1(evt)
-
-  def error(e: Throwable): F[Unit] = {
-    val x =topic.publish(Stream.raiseError(e))
+  def publish(evt: Event): F[Either[Topic.Closed, Unit]] = {
+    val pub = topic.publish1(Right(evt))
+    evt match {
+      case EError(_, _, _) => pub >> stop()
+      case EDone(_, _) => pub >> stop()
+      case _ => pub
     }
+  }
+
+  def error(e: Throwable): F[Either[Topic.Closed, Unit]] = topic.publish1(Left(e)) >> stop()
 
   /**
     * Performs any cleaning up required when the stream is finished.
@@ -38,21 +43,14 @@ case class Publisher[F[_]](topic: Topic[F, Event], maxQueued: Int)(using MonadCa
   def subscribe(subscriber: EventHandler): F[Unit] = topic.subscribeAwait(maxQueued).use { sub => 
     for {
       _ <- subscriber.onInit(this)
-    } yield sub.evalMap (
-      evt => subscriber.onEvent(evt)
+    } yield sub.evalMap ( 
+      evt => evt match {
+        case Left(e) => subscriber.onFail(e, this)
+        case Right(e) => subscriber.onEvent(e)
+      }
     ).handleErrorWith (
       ex => Stream.eval(subscriber.onFail(ex, this))
     )
   }
-
-  /**
-    * Wrapper method for [[doPublish]] that also stops the stream upon final events.
-    *
-    * @param evt
-    */
- /* def publish(evt: Event): Unit = {
-    doPublish(evt)
-    if isFinalEvent(evt) then stopStream()
-  }*/
 }
 
