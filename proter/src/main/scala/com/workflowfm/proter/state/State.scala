54d16941c591b82a97e4e09111d90a9bc30f75e9
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-03-22 23:44:11 +0000

    Adds `State.startCase` and `stopCase`

--- proter/src/main/scala/com/workflowfm/proter/state/State.scala
+++ proter/src/main/scala/com/workflowfm/proter/state/State.scala
@@ -21,7 +21,7 @@ case class State[F[_] : Monad, C[T] <: Iterable[T]](
   tasks: C[TaskInstance],
   cases: Map[String, CaseRef[F]], 
   resources: ResourceMap,
-  waiting: Map[CaseRef[F], Seq[TaskInstance]], 
+  waiting: Map[String, Seq[TaskInstance]], 
   abortedTasks: HashSet[UUID],
 ) {
 
@@ -100,6 +100,76 @@ case class State[F[_] : Monad, C[T] <: Iterable[T]](
   def addCasesNow(publisher: Publisher[F], cases: Seq[CaseRef[F]]): F[State[F, C]] = addCases(publisher, cases.map((time, _)))
 
 
+
+
+  /**
+    * Start a [[CaseRef]].
+    *
+    * Once the case starts, we expect to hear from it in case it wants to add some [[Task]]s.
+    * We therefore add it to the waiting queue.
+    *
+    * Publishes a [[com.workflowfm.proter.events.ESimStart ESimStart]].
+    *
+    * @group simulations
+    * @param caseRef
+    *   The [[CaseRef]] to start.
+    */
+  protected def startCase(publisher: Publisher[F], caseRef: CaseRef[F]): F[State[F, C]] = 
+    publishThen(publisher, ECaseStart(id, time, caseRef.caseName))(
+      copy(
+        waiting = waiting + (caseRef.caseName -> Seq()),
+        cases = cases + (caseRef.caseName -> caseRef)
+      ))
+    // simulation.run() !!!
+
+  /**
+    * Stops a case when it is done.
+    *
+    *   - Removes the simulation from the list of running simulations and the waiting list.
+    *   - Removes any [[Lookahead]] from the [[schedule.Scheduler Scheduler]].
+    *   - Publishes a [[com.workflowfm.proter.events.ESimEnd ESimEnd]].
+    *   - Calls [[ready]] to handle the fact that we no longer need to wait for this simulation.
+    *
+    * @group simulations
+    * @param name
+    *   The name of the completed simulation.
+    * @param result
+    *   A string representation of the output of the simulation.
+    */
+  protected def stopCase(publisher: Publisher[F], name: String, result: String): F[State[F, C]] = 
+    publishThen(publisher, ECaseEnd(id, time, name, result))(
+      copy(
+        waiting = waiting - name,
+        cases = cases - name,
+      ))
+    //scheduler.removeLookahead(name)
+    // log.debug(s"[COORD:$time] Finished: [$name]")
+    //ready(name)
+  
+
+/*
+  /**
+    * Handles a [[SimDone]] response from a simulation, indicating that it has completed.
+    *
+    * Calls [[stopSimulation]] according to the reported success or failure result.
+    *
+    * @group simulations
+    * @param simulation
+    *   The name of the simulation
+    * @param result
+    *   The result of completion
+    */
+  protected def caseDone(caseName: String, result: Try[Any]): Unit = {
+    result match {
+      case Success(res) => {
+        stopSimulation(simulation, res.toString)
+      }
+      case Failure(ex) => {
+        stopSimulation(simulation, ex.getLocalizedMessage)
+      }
+    }
+  }
+ */
   def tick(publisher: Publisher[F]): F[StateResult[F, C]] = {
     if !waiting.isEmpty // still waiting for responses!
     then for {
