6f51f283700793d02a0cc2f1d40ad9764b08b3ea
Petros Papapanagiotou <petrospapapan@gmail.com>
2022-03-31 01:20:46 +0100

    Makes progress with State

--- proter/src/main/scala/com/workflowfm/proter/state/State.scala
+++ proter/src/main/scala/com/workflowfm/proter/state/State.scala
@@ -29,6 +29,125 @@ case class Simulationx[F[_]](
 }
 
 object Simulationx {
+
+  /**
+    * Start a [[CaseRef]].
+    *
+    * Once the case starts, we expect to hear from it in case it wants to add some [[Task]]s.
+    * We therefore add it to the waiting queue.
+    *
+    * Publishes a [[com.workflowfm.proter.events.ESimStart ESimStart]].
+    *
+    * @group simulations
+    * @param caseRef
+    *   The [[CaseRef]] to start.
+    */
+  def startCase[F[_]](caseRef: CaseRef[F]): State[Simulationx[F], Event] = State ( sim =>
+    (sim.copy(
+      waiting = sim.waiting + (caseRef.caseName -> Seq()),
+      cases = sim.cases + (caseRef.caseName -> caseRef)
+    ),
+      ECaseStart(sim.id, sim.time, caseRef.caseName))
+  )
+
+  /**
+    * Stops a case when it is done.
+    *
+    *   - Removes the simulation from the list of running simulations and the waiting list.
+    *   - Removes any [[Lookahead]] from the [[schedule.Scheduler Scheduler]].
+    *   - Publishes a [[com.workflowfm.proter.events.ESimEnd ESimEnd]].
+    *   - Calls [[ready]] to handle the fact that we no longer need to wait for this simulation.
+    *
+    * @group simulations
+    * @param name
+    *   The name of the completed simulation.
+    * @param result
+    *   A string representation of the output of the simulation.
+    */
+  def stopCase[F[_]](name: String, result: String): State[Simulationx[F], Event] = State ( sim => 
+    (sim.copy(
+      waiting = sim.waiting - name,
+      cases = sim.cases - name,
+    ),
+      ECaseEnd(sim.id, sim.time, name, result))
+  )
+    //scheduler.removeLookahead(name)
+    // log.debug(s"[COORD:$time] Finished: [$name]")
+    //ready(name)
+  
+
+/*
+  /**
+    * Handles a [[SimDone]] response from a simulation, indicating that it has completed.
+    *
+    * Calls [[stopSimulation]] according to the reported success or failure result.
+    *
+    * @group simulations
+    * @param simulation
+    *   The name of the simulation
+    * @param result
+    *   The result of completion
+    */
+  protected def caseDone(caseName: String, result: Try[Any]): Unit = {
+    result match {
+      case Success(res) => {
+        stopSimulation(simulation, res.toString)
+      }
+      case Failure(ex) => {
+        stopSimulation(simulation, ex.getLocalizedMessage)
+      }
+    }
+  }
+ */
+  def tick[F[_]](): State[Simulationx[F], Seq[Event]] = State ( sim => 
+    if !sim.waiting.isEmpty // still waiting for responses!
+    then (sim, Seq(sim.error(s"Called `tick()` even though I am still waiting for: ${sim.waiting}")))
+    else if (sim.events.isEmpty && sim.tasks.isEmpty && sim.cases.isEmpty) // finished!
+    then (sim, Seq(EDone(sim.id, sim.time))) 
+/*  
+    else if !events.isEmpty // Are events pending?
+    then { 
+      processing = true
+
+      // Grab the first event
+      val firstEvent = events.head
+
+      // Did we somehow go past the event time? This should never happen.
+      if firstEvent.time < time then {
+        publish(EError(id, time, s"Unable to handle past event for time: [${firstEvent.time}]"))
+      } else {
+        // Jump ahead to the event time. This is a priority queue so we shouldn't skip any events
+        time = firstEvent.time
+
+        // Dequeue all the events that need to happen now
+        val eventsToHandle = dequeueEvents(firstEvent.time)
+
+        eventsToHandle.flatMap(filterFinishingTasks).groupBy(_.simulation).foreach(stopTasks)
+
+        // Handle the event
+        eventsToHandle foreach handleDiscreteEvent
+
+        processing = false
+        // If we are not waiting for anything, continue
+        if waiting.isEmpty then {
+          allocateTasks()
+          tick()
+        }
+      }
+
+    } else if waiting.isEmpty && !scheduler.noMoreTasks() then { // this may happen if handleDiscreteEvent fails
+      allocateTasks()
+      tick()
+    } // else {
+    // publish(EError(self, time, "No tasks left to run, but simulations have not finished."))
+    // }*/
+    else (sim, Seq())
+  )
+
+}
+
+
+trait CaseState {
 /*
   private def publishThen[T](publisher: Publisher[F], events: Event*) (v: T): F[T] =
     for {
@@ -91,11 +210,6 @@ object Simulationx {
   def addCases[F[_]](cases: Seq[(Long, CaseRef[F])]): State[Simulationx[F], Seq[Event]] =
     cases.map( (t, c) => addCase(t, c) ).sequence
 
-/*    case Nil => Monad[F].pure(this)
-    case (t, c) :: cs => addCase(publisher, t, c).flatMap(_.addCases(publisher,cs)) // TODO this can be more efficient
-  }
- *//*
-
   /**
     * Add multiple simulations to be run in the current virtual time.
     *
@@ -103,129 +217,7 @@ object Simulationx {
     * @param sims
     *   A sequence of [[Simulation]]s.
     */
-  def addCasesNow(publisher: Publisher[F], cases: Seq[CaseRef[F]]): F[Simulation[F, C]] = addCases(publisher, cases.map((time, _)))
-
-
-
-
-  /**
-    * Start a [[CaseRef]].
-    *
-    * Once the case starts, we expect to hear from it in case it wants to add some [[Task]]s.
-    * We therefore add it to the waiting queue.
-    *
-    * Publishes a [[com.workflowfm.proter.events.ESimStart ESimStart]].
-    *
-    * @group simulations
-    * @param caseRef
-    *   The [[CaseRef]] to start.
-    */
-  protected def startCase(publisher: Publisher[F], caseRef: CaseRef[F]): F[Simulation[F, C]] = 
-    publishThen(publisher, ECaseStart(id, time, caseRef.caseName))(
-      copy(
-        waiting = waiting + (caseRef.caseName -> Seq()),
-        cases = cases + (caseRef.caseName -> caseRef)
-      ))
-    // simulation.run() !!!
-
-  /**
-    * Stops a case when it is done.
-    *
-    *   - Removes the simulation from the list of running simulations and the waiting list.
-    *   - Removes any [[Lookahead]] from the [[schedule.Scheduler Scheduler]].
-    *   - Publishes a [[com.workflowfm.proter.events.ESimEnd ESimEnd]].
-    *   - Calls [[ready]] to handle the fact that we no longer need to wait for this simulation.
-    *
-    * @group simulations
-    * @param name
-    *   The name of the completed simulation.
-    * @param result
-    *   A string representation of the output of the simulation.
-    */
-  protected def stopCase(publisher: Publisher[F], name: String, result: String): F[Simulation[F, C]] = 
-    publishThen(publisher, ECaseEnd(id, time, name, result))(
-      copy(
-        waiting = waiting - name,
-        cases = cases - name,
-      ))
-    //scheduler.removeLookahead(name)
-    // log.debug(s"[COORD:$time] Finished: [$name]")
-    //ready(name)
-  
-
-/*
-  /**
-    * Handles a [[SimDone]] response from a simulation, indicating that it has completed.
-    *
-    * Calls [[stopSimulation]] according to the reported success or failure result.
-    *
-    * @group simulations
-    * @param simulation
-    *   The name of the simulation
-    * @param result
-    *   The result of completion
-    */
-  protected def caseDone(caseName: String, result: Try[Any]): Unit = {
-    result match {
-      case Success(res) => {
-        stopSimulation(simulation, res.toString)
-      }
-      case Failure(ex) => {
-        stopSimulation(simulation, ex.getLocalizedMessage)
-      }
-    }
-  }
- */
-  def tick(publisher: Publisher[F]): F[StateResult[F, C]] = {
-    if !waiting.isEmpty // still waiting for responses!
-    then for {
-      _ <- publisher.publish(
-        EError(id, time, s"Called `tick()` even though I am still waiting for: $waiting") 
-      ) } yield (StateDone(this))
-
-    else if (events.isEmpty && tasks.isEmpty && cases.isEmpty) // finished!
-    then for {
-      _ <- publisher.publish(EDone(id, time)) 
-    } yield (StateDone(this)) 
-/*  
-    else if !events.isEmpty // Are events pending?
-    then { 
-      processing = true
-
-      // Grab the first event
-      val firstEvent = events.head
-
-      // Did we somehow go past the event time? This should never happen.
-      if firstEvent.time < time then {
-        publish(EError(id, time, s"Unable to handle past event for time: [${firstEvent.time}]"))
-      } else {
-        // Jump ahead to the event time. This is a priority queue so we shouldn't skip any events
-        time = firstEvent.time
-
-        // Dequeue all the events that need to happen now
-        val eventsToHandle = dequeueEvents(firstEvent.time)
-
-        eventsToHandle.flatMap(filterFinishingTasks).groupBy(_.simulation).foreach(stopTasks)
-
-        // Handle the event
-        eventsToHandle foreach handleDiscreteEvent
-
-        processing = false
-        // If we are not waiting for anything, continue
-        if waiting.isEmpty then {
-          allocateTasks()
-          tick()
-        }
-      }
-
-    } else if waiting.isEmpty && !scheduler.noMoreTasks() then { // this may happen if handleDiscreteEvent fails
-      allocateTasks()
-      tick()
-    } // else {
-    // publish(EError(self, time, "No tasks left to run, but simulations have not finished."))
-    // }*/
-    else Monad[F].pure(StateDone(this))
-  }
-
- */
+  def addCasesNow[F[_]](cases: Seq[CaseRef[F]]): State[Simulationx[F], Seq[Event]] = State ( sim => 
+    addCases(cases.map((sim.time, _))).run(sim).value
+  )
 }
